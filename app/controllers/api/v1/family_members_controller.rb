4195ab84778c1bec9c9d02e7480ccb6e31f82bf5
Jiapeng Ji <jjp4ever@gmail.com>
2020-05-26 02:30:22 -0400

    Add in family member retrieval and delete endpoints

--- app/controllers/api/v1/family_members_controller.rb
+++ app/controllers/api/v1/family_members_controller.rb
@@ -26,7 +26,55 @@ module Api
         end
       end
 
+      swagger_api :index do
+        summary 'Retrieves a parent user\'s family members'
+        param  :query, 'user_email', :string, :required, 'Parent user email'
+        response :unauthorized
+        response :internal_server_error
+        response :ok
+      end
+
+      def index
+        begin
+          identified_parent_user = User.where(email: user_email).first
+          raise 'Attempt to retrieve family members with an unauthorized user' unless current_user == identified_parent_user
+          render json: { family_members: identified_parent_user.parent.family_members.map(&:as_serialized_hash) }, status: :ok
+        rescue => exception
+          render json: { errors: { family_member_retrieval_errors: exception.to_s } }, status: :internal_server_error
+        end
+      end
+
+      swagger_api :destroy do
+        summary 'Deletes a parent user\'s family member (student)'
+        param :query, 'user_email', :string, :required, 'Parent user email'
+        param :query, 'family_member[first_name]', :string, :required, 'Deleting family member student first name'
+        param :query, 'family_member[last_name]', :string, :required, 'Deleting family member student last name'
+        response :unauthorized
+        response :internal_server_error
+        response :ok
+      end
+
+      def destroy
+        begin
+          family_member = FamilyMember.find(params[:id])
+          raise 'Attempt to delete a family member with an unauthorized user' unless current_user == family_member&.parent&.user
+
+          ActiveRecord::Base.transaction do
+            family_member.destroy
+            family_member.student.destroy
+          end
+
+          render json: { family_member: family_member.as_serialized_hash }, status: :ok
+        rescue => exception
+          render json: { errors: { family_member_deletion_error: exception.to_s } }, status: :internal_server_error
+        end
+      end
+
       private
+
+      def user_email
+        params.require(:user_email)
+      end
 
       def student_params
         params.
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :authentications, only: [:create]
       resources :authentication_renewals, only: [:create]
-      resources :family_members, only: [:create]
+      resources :family_members, only: [:create, :index, :destroy]
       resources :sign_ups, only: [:create]
       resources :user_profiles, only: [:index]
       resources :user_profile_changes, only: [:create]
