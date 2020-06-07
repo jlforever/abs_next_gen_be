d5870042a3f39cadf45c677ceba91c1b34012418
Jiapeng Ji <jjp4ever@gmail.com>
2020-06-07 18:19:01 -0400

    config action mailer using mailgun and add in a boilerplate registrations mailer
    Add in registration test email template files
    
    Change to use .com for jjp4ever

--- config/configatron/defaults.rb
+++ config/configatron/defaults.rb
@@ -1,1 +1,8 @@
 configatron.redis_url = 'redis://localhost:6379'
+
+configatron.mailer.smtp_settings.address = 'localhost'
+configatron.mailer.smtp_settings.port = 1025
+configatron.mailer.smtp_settings.user_name = nil
+configatron.mailer.smtp_settings.password = nil
+configatron.mailer.smtp_settings.delivery_method = :smtp
+configatron.mailer.smtp_settings.domain = 'localhost:9292'
