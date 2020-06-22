d91f7741a7068bdef4068bf6c1cf426f7e5e45b7
Jiapeng Ji <jjp4ever@gmail.com>
2020-06-22 13:00:27 -0400

    Switch staging to use sendgrid

--- config/configatron/staging.rb
+++ config/configatron/staging.rb
@@ -2,7 +2,7 @@ configatron.redis_url = ENV['REDIS_URL']
 
 configatron.mailer.smtp_settings.port = 587
 configatron.mailer.smtp_settings.address = 'smtp.mailgun.org'
-configatron.mailer.smtp_settings.user_name = ENV['MAILGUN_SMTP_LOGIN']
-configatron.mailer.smtp_settings.password = ENV['MAILGUN_SMTP_PASSWORD']
+configatron.mailer.smtp_settings.user_name = ENV['SENDGRID_USERNAME']
+configatron.mailer.smtp_settings.password = ENV['SENDGRID_PASSWORD']
 configatron.mailer.smtp_settings.delivery_method = :smtp
 configatron.mailer.smtp_settings.domain = 'staging.alphabetaacademy.org'
