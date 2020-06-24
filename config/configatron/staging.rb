e634cfc22e860af3812d18203190547f7af3ac30
Jiapeng Ji <jjp4ever@gmail.com>
2020-06-24 02:32:50 -0400

    Change send email address to use sendgrid

--- config/configatron/staging.rb
+++ config/configatron/staging.rb
@@ -1,7 +1,7 @@
 configatron.redis_url = ENV['REDIS_URL']
 
 configatron.mailer.smtp_settings.port = 587
-configatron.mailer.smtp_settings.address = 'smtp.mailgun.org'
+configatron.mailer.smtp_settings.address = 'smtp.sendgrid.net'
 configatron.mailer.smtp_settings.user_name = ENV['SENDGRID_USERNAME']
 configatron.mailer.smtp_settings.password = ENV['SENDGRID_PASSWORD']
 configatron.mailer.smtp_settings.delivery_method = :smtp
